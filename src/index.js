95bfb9a600b0f30fb32b372944be51e7da9bace2
david.monz <david.monz@macio.de>
2021-11-13 16:19:15 +0100

    add css to webpack, add prettier, add tsconfig

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import './style.css';
+
 function component() {
     const element = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = "Hello World!"
+    element.innerHTML = 'Hello World!';
+    element.classList.add('hello');
 
     return element;
 }
