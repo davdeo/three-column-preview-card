56e42ac40f8c62a387850eb3d5e9a74c60e8cd21
david.monz <david.monz@macio.de>
2021-11-13 16:33:30 +0100

    add font and image support to webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './style.css';
+import './assets/style/style.css';
 
 function component() {
     const element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,23 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     },
-    mode: "development",
+    mode: 'development',
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader']
             },
-        ],
-    },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
 };
