d0ab78f329971d9f7b2348ef78843210129f99f6
david.monz <david.monz@macio.de>
2021-11-18 19:16:53 +0100

    initial index.ts

---
+++ src/components/three-column-card/index.ts
@@ -0,0 +1,9 @@
+export default function threeCardComponent() {
+    const element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = 'Hello World!';
+    element.classList.add('hello');
+
+    return element;
+}
